6e1de8fa90a6fbb19cf12193e44f26e6b1594e51
treneese <treneesejohnson9012@gmail.com>
2024-04-02 20:07:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+//const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
